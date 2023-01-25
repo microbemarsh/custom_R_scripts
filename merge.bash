6ed634b7cb52fcb405643b27135bb5bb4e049064
Austin Marshall <75810734+microbemarsh@users.noreply.github.com>
2023-01-25 17:38:28 -0500

    Add files via upload

---
+++ merge.bash
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+seqkit scat -j 8 -f /home/marshaag/AAAR_paper/first_rel_abund/barcode01/pass > /home/marshaag/AAAR_paper/S1.fq
+seqkit scat -j 8 -f /home/marshaag/AAAR_paper/first_rel_abund/barcode02/pass > /home/marshaag/AAAR_paper/S2.fq
+seqkit scat -j 8 -f /home/marshaag/AAAR_paper/first_rel_abund/barcode04/pass > /home/marshaag/AAAR_paper/S3.fq
+seqkit scat -j 8 -f /home/marshaag/AAAR_paper/first_rel_abund/barcode06/pass > /home/marshaag/AAAR_paper/S4.fq
+seqkit scat -j 8 -f /home/marshaag/AAAR_paper/first_rel_abund/barcode07/pass > /home/marshaag/AAAR_paper/S5.fq
